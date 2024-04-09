d757328c9e3b51db686fc0a03695a0eca81521c2
Jacob Toftgaard <jacobjshelp@gmail.com>
2024-04-09 07:26:05 +0400

    redid sevenBoom

--- edabitHard/sevenBoom.ts
+++ edabitHard/sevenBoom.ts
@@ -3,16 +3,18 @@ function sevenBoom(arr: number[]): string {
     const element = arr[i]
     const asString = String(element)
     for (let j = 0; j < asString.length; j++) {
-      const number = asString[j]
-      if (number === '7') {
+      const digit = asString[j]
+      if (digit === '7') {
         return 'Boom!'
       }
     }
   }
-
   return 'there is no 7 in the array'
 }
 
 console.log(sevenBoom([1, 2, 3, 4, 5, 6, 7]) === 'Boom!')
+// 7 contains the number seven.
 console.log(sevenBoom([8, 6, 33, 100]) === 'there is no 7 in the array')
+// None of the items contain 7 within them.
 console.log(sevenBoom([2, 55, 60, 97, 86]) === 'Boom!')
+// 97 contains the number seven.
