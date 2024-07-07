aa8e7e14c8646bf0d22df09181991276b21dc82f
Jacob Toftgaard <jacobjshelp@gmail.com>
2024-07-07 09:36:35 +0400

    added solution for oddOrEvenLength exercise

--- homeMade/oddOrEvenLength.ts
+++ homeMade/oddOrEvenLength.ts
@@ -20,3 +20,24 @@ const interestingWords: string[] = [
 	'Mystery',
 	'Wonder',
 ];
+
+class App {
+	public main() {
+		interestingWords.forEach((word) => {
+			const length = word.length;
+			if (this.isEven(length)) {
+				console.log(`${word} is even with length ${length}`);
+			} else {
+				console.log(`${word} is odd with length ${length}`);
+			}
+		});
+	}
+
+	isEven(number: number) {
+		return number % 2 === 0;
+	}
+}
+
+const app = new App();
+
+app.main();
