5e0f19072a7f4bb84f6420142cb23fcfe423c22d
Jacob Toftgaard <jacobjshelp@gmail.com>
2024-07-25 15:48:05 +0400

    working example (needs refactoring)

--- homeMade/shopExercise/shop.js
+++ homeMade/shopExercise/shop.js
@@ -32,15 +32,46 @@ function showAvailableProducts() {
   console.log('--------------------------------');
 }
 
+function inputIsValid(input) {
+  const arguments = input.split(' ')
+  const product = arguments[0]
+  const amount = Number(arguments[1])
+  for (let i = 0; i < products.length; i++) {
+    const p = products[i];
+    if (p.name === product && amount <= p.stock) {
+      return true
+    }
+  }
+  return false
+}
+
 async function askForInput() {
   const answer = await input({ message: 'What product would you like?' })
-  console.log('You asked for', answer);
+  try {
+    if (inputIsValid(answer)) {
+      return answer
+    } else {
+      console.log('Your input was not valid');
+    }
+  } catch (error) {
+    console.log('An error happened because your input was not valid');
+  }
 }
 
 async function start() {
   printWelcomeMessage()
-  showAvailableProducts()
-  await askForInput()
+
+  while (true) {
+    showAvailableProducts()
+    const answer = await askForInput()
+    const arguments = answer.split(' ')
+    const product = arguments[0]
+    const amount = Number(arguments[1])
+    const indexOfWantedProduct = products.findIndex(p => p.name === product)
+    const newAmount = products[indexOfWantedProduct].stock - amount
+    products[indexOfWantedProduct] = { ...products[indexOfWantedProduct], stock: newAmount }
+    showAvailableProducts()
+  }
 }
 
 start()
